c0cca1e3e003d0264e7bba36877c5dd7711a6894
depseee <raysikh1990@mail.ru>
2023-09-11 13:55:36 +0300

    Email Verification

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
-from .models import User
+import uuid
+from datetime import timedelta
+
+from django.utils.timezone import now
+
+from .models import User, EmailVerification
 
 
 class UserLoginForm(AuthenticationForm):
@@ -41,6 +46,13 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=True)
+        expiration = now() + timedelta(hours=48)
+        record = EmailVerification.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
+        record.send_verification_email()
+        return user
 
 
 class UserProfileForm(UserChangeForm):
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('register', views.UserRegistrationView.as_view(), name='register'),
     path('login', views.UserLoginView.as_view(), name='login'),
     path('profile/<int:pk>/', login_required(views.UserProfileView.as_view()), name='profile'),
-    path('logout', views.UserLoginView.as_view(), name='logout'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
